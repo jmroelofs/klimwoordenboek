dd085beb98546dd6e9d88cee9f081cba98b59c1a
Jan Martin roelofs <jmartinr@home.nl>
2025-11-09 21:57:33 +0100

    use more private elements

--- js/columns.mjs
+++ js/columns.mjs
@@ -1,36 +1,36 @@
 class Columns {
-    animationFrameID = null;
+    #animationFrameID = null;
 
-    root = document.documentElement
-    column = document.getElementsByClassName('continuous-column')[0];
-    spacer = this.column
+    #root = document.documentElement
+    #column = document.getElementsByClassName('continuous-column')[0];
+    #spacer = this.#column
         ? document.createElement('div')
         : null;
-    lastParagraph = this.column?.lastElementChild;    
-    firstParagraph = this.column?.firstElementChild;
-    firstParagraphHeight = this.firstParagraph?.getBoundingClientRect().height;
-    secondParagraph = this.firstParagraph?.nextElementSibling;
-    secondParagraphLineHeight = this.secondParagraph
-        ? parseInt(window.getComputedStyle(this.secondParagraph).getPropertyValue('line-height'))
+    #lastParagraph = this.#column?.lastElementChild;    
+    #firstParagraph = this.#column?.firstElementChild;
+    #firstParagraphHeight = this.#firstParagraph?.getBoundingClientRect().height;
+    #secondParagraph = this.#firstParagraph?.nextElementSibling;
+    #secondParagraphLineHeight = this.#secondParagraph
+        ? parseInt(window.getComputedStyle(this.#secondParagraph).getPropertyValue('line-height'))
         : null;
 
-    offsetOld = 0;
-    heightOld = 1;
+    #offsetOld = 0;
+    #heightOld = 1;
 
-    mediaQuery = window.matchMedia('screen and (min-width: 801px) and (min-device-width: 750px)');
-    matchesMedia = this.mediaQuery.matches;
+    #mediaQuery = window.matchMedia('screen and (min-width: 801px) and (min-device-width: 750px)');
+    #matchesMedia = this.#mediaQuery.matches;
 
-    diffMoreThan = (x, y, z) => Math.abs(x - y) > z;
-    roundNearest = (x, y) => y * Math.round(x / y);
+    #diffMoreThan = (x, y, z) => Math.abs(x - y) > z;
+    #roundNearest = (x, y) => y * Math.round(x / y);
 
-    flowColumns = () => {
+    #flowColumns = () => {
         // offset of page readings
         const
-            offsetNeeded = this.roundNearest(window.scrollY, this.secondParagraphLineHeight),
-            offsetDifference = offsetNeeded - this.offsetOld,
+            offsetNeeded = this.#roundNearest(window.scrollY, this.#secondParagraphLineHeight),
+            offsetDifference = offsetNeeded - this.#offsetOld,
             // column readings
-            {top: spacerTop, bottom: spacerBottom} = this.spacer.getBoundingClientRect(),
-            {bottom: lastParagraphBottom} = [...this.lastParagraph.getClientRects()].at(-1);
+            {top: spacerTop, bottom: spacerBottom} = this.#spacer.getBoundingClientRect(),
+            {bottom: lastParagraphBottom} = [...this.#lastParagraph.getClientRects()].at(-1);
 
         // calculate height of spacer
         let calculatedHeight;
@@ -46,51 +46,51 @@ class Columns {
             // we are undershooting
             // console.log('[flowColumns] undershooting');
 
-            const heightNeeded =  this.column.getBoundingClientRect().bottom - spacerTop;
-            calculatedHeight = (2 * heightNeeded) - this.heightOld - offsetDifference;
+            const heightNeeded =  this.#column.getBoundingClientRect().bottom - spacerTop;
+            calculatedHeight = (2 * heightNeeded) - this.#heightOld - offsetDifference;
 
         }
 
         // add an extra margin on top: with a maximum of the height of the biggest child and a minimum of half a line
         const
             safetyMargin = Math.max(
-                Math.min(1, calculatedHeight / this.root.clientHeight) * this.firstParagraphHeight,
-                this.secondParagraphLineHeight / 2
+                Math.min(1, calculatedHeight / this.#root.clientHeight) * this.#firstParagraphHeight,
+                this.#secondParagraphLineHeight / 2
             ),
             heightNew = Math.max(1, calculatedHeight + safetyMargin);
 
         // adjust offset
-        this.column.style.setProperty('--column-offset', `${offsetNeeded}px`);
-        this.offsetOld = offsetNeeded
+        this.#column.style.setProperty('--column-offset', `${offsetNeeded}px`);
+        this.#offsetOld = offsetNeeded
 
         // adjust height of spacer
-        if (this.diffMoreThan(heightNew, this.heightOld, 1)) {
-            this.spacer.style.height = `${heightNew}px`;
-            this.heightOld = heightNew;
+        if (this.#diffMoreThan(heightNew, this.#heightOld, 1)) {
+            this.#spacer.style.height = `${heightNew}px`;
+            this.#heightOld = heightNew;
         }
 
-        this.animationFrameID = null;
+        this.#animationFrameID = null;
     };
 
-    handleEvent = event => {
-        if (this.animationFrameID) {
+    #handleEvent = event => {
+        if (this.#animationFrameID) {
             console.log(`[flowColumns] resetting animation in ${event.type} event`);
-            window.cancelAnimationFrame(this.animationFrameID);
+            window.cancelAnimationFrame(this.#animationFrameID);
         }
 
-        if (this.matchesMedia) {
-            this.animationFrameID = window.requestAnimationFrame(this.flowColumns);
+        if (this.#matchesMedia) {
+            this.#animationFrameID = window.requestAnimationFrame(this.#flowColumns);
         }        
     };
   
     setupFlow = () => {
-        if (this.column) {
-            this.spacer.id = 'spacer';
-            this.column.prepend(this.spacer);
-            this.mediaQuery.addEventListener('change', event => this.matchesMedia = event.matches);
-            this.handleEvent({ type: 'init' });
+        if (this.#column) {
+            this.#spacer.id = 'spacer';
+            this.#column.prepend(this.#spacer);
+            this.#mediaQuery.addEventListener('change', event => this.#matchesMedia = event.matches);
+            this.#handleEvent({ type: 'init' });
             ['scroll', 'resize'].forEach(event =>
-                window.addEventListener(event, this.handleEvent, { passive: true, capture: true })
+                window.addEventListener(event, this.#handleEvent, { passive: true, capture: true })
             );
         }
     }
