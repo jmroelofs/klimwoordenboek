659c126c1f9101a1b6aeedb1217cb36419b1f30a
Jan Martin roelofs <jmartinr@home.nl>
2025-11-09 21:27:07 +0100

    use private elements

--- js/activeLink.mjs
+++ js/activeLink.mjs
@@ -1,23 +1,23 @@
 class ActiveLink {
-    allLinks;
-    activeLink;
+    #allLinks;
+    #activeLink;
 
-    setActive = event => {
-        this.activeLink?.classList.remove('active-link');
+    #setActive = event => {
+        this.#activeLink?.classList.remove('active-link');
 
-        this.allLinks.some(link => {
+        this.#allLinks.some(link => {
             if (link.href === event.newURL) {
                 link.classList.add('active-link');
-                this.activeLink = link;
+                this.#activeLink = link;
                 return true
             }
         })
     }
 
     setupLinks = links => {
-        this.allLinks = Array.from(links);
-        this.setActive({ newURL: window.location.href });
-        window.addEventListener('hashchange', this.setActive, { passive: true })
+        this.#allLinks = Array.from(links);
+        this.#setActive({ newURL: window.location.href });
+        window.addEventListener('hashchange', this.#setActive, { passive: true })
     }
 }
 
