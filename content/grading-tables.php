c5c1684bc7786d7e5c76327d965891034f471d61
Jan Martin roelofs <jmartinr@home.nl>
2026-04-05 22:33:19 +0200

    improve coding of width in grading tables

--- content/grading-tables.php
+++ content/grading-tables.php
@@ -8,174 +8,174 @@
 <h3>Routewaarderingen</h3>
 <section class="grading-table">
   <p><a href="waarderingen#cotation">Frans</a></p>
-  <table style="width:800px;">
+  <table>
     <tr>
-      <td style="width:32px">5</td>
-      <td style="width:32px">5<sup>+</sup></td>
-      <td style="width:32px">6a</td>
-      <td style="width:32px">6a<sup>+</sup></td>
-      <td style="width:32px">6b</td>
-      <td style="width:32px">6b<sup>+</sup></td>
-      <td style="width:32px">6c</td>
-      <td style="width:32px">6c<sup>+</sup></td>
-      <td style="width:32px">7a</td>
-      <td style="width:32px">7a<sup>+</sup></td>
-      <td style="width:32px">7b</td>
-      <td style="width:32px">7b<sup>+</sup></td>
-      <td style="width:32px">7c</td>
-      <td style="width:32px">7c<sup>+</sup></td>
-      <td style="width:32px">8a</td>
-      <td style="width:32px">8a<sup>+</sup></td>
-      <td style="width:32px">8b</td>
-      <td style="width:32px">8b<sup>+</sup></td>
-      <td style="width:32px">8c</td>
-      <td style="width:32px">8c<sup>+</sup></td>
-      <td style="width:32px">9a</td>
-      <td style="width:32px">9a+</td>
-      <td style="width:32px">9b</td>
-      <td style="width:32px">9b+</td>
-      <td style="width:32px">9c</td>
+      <td class="w-8">5</td>
+      <td class="w-8">5<sup>+</sup></td>
+      <td class="w-8">6a</td>
+      <td class="w-8">6a<sup>+</sup></td>
+      <td class="w-8">6b</td>
+      <td class="w-8">6b<sup>+</sup></td>
+      <td class="w-8">6c</td>
+      <td class="w-8">6c<sup>+</sup></td>
+      <td class="w-8">7a</td>
+      <td class="w-8">7a<sup>+</sup></td>
+      <td class="w-8">7b</td>
+      <td class="w-8">7b<sup>+</sup></td>
+      <td class="w-8">7c</td>
+      <td class="w-8">7c<sup>+</sup></td>
+      <td class="w-8">8a</td>
+      <td class="w-8">8a<sup>+</sup></td>
+      <td class="w-8">8b</td>
+      <td class="w-8">8b<sup>+</sup></td>
+      <td class="w-8">8c</td>
+      <td class="w-8">8c<sup>+</sup></td>
+      <td class="w-8">9a</td>
+      <td class="w-8">9a+</td>
+      <td class="w-8">9b</td>
+      <td class="w-8">9b+</td>
+      <td class="w-8">9c</td>
     </tr>
   </table>
   <p><a href="waarderingen#alpin">Frans, meerdere touwlengten</a></p>
   <table>
     <tr>
-      <td style="width:32px">TD</td>
-      <td style="width:32px">TD<sup>+</sup></td>
-      <td style="width:64px">ED<sup>&minus;</sup></td>
-      <td style="width:64px">ED</td>
-      <td style="width:64px">ED<sup>+</sup></td>
-      <td style="width:64px">ABO<sup>&minus;</sup></td>
-      <td style="width:64px">ABO</td>
-      <td style="width:192px">ABO<sup>+</sup></td>
+      <td class="w-8">TD</td>
+      <td class="w-8">TD<sup>+</sup></td>
+      <td class="w-16">ED<sup>&minus;</sup></td>
+      <td class="w-16">ED</td>
+      <td class="w-16">ED<sup>+</sup></td>
+      <td class="w-16">ABO<sup>&minus;</sup></td>
+      <td class="w-16">ABO</td>
+      <td class="w-48">ABO<sup>+</sup></td>
     </tr>
   </table>
   <p><a href="waarderingen#UIAA">UIAA/Duits</a></p>
   <table>
     <tr>
-      <td style="width:32px">V<sup>+</sup></td>
-      <td style="width:20px">VI<sup>&minus;</sup></td>
-      <td style="width:20px">VI</td>
-      <td style="width:24px">VI<sup>+</sup></td>
-      <td style="width:32px">VII<sup>&minus;</sup></td>
-      <td style="width:32px">VII</td>
-      <td style="width:48px">VII<sup>+</sup></td>
-      <td style="width:48px">VIII<sup>&minus;</sup></td>
-      <td style="width:44px">VIII</td>
-      <td style="width:44px">VIII<sup>+</sup></td>
-      <td style="width:40px">IX<sup>&minus;</sup></td>
-      <td style="width:40px">IX</td>
-      <td style="width:48px">IX<sup>+</sup></td>
-      <td style="width:48px">X<sup>&minus;</sup></td>
-      <td style="width:40px">X</td>
-      <td style="width:40px">X<sup>+</sup></td>
-      <td style="width:40px">XI<sup>&minus;</sup></td>
-      <td style="width:32px">XI</td>
-      <td style="width:32px">XI<sup>+</sup></td>
-      <td style="width:32px">XII<sup>&minus;</sup></td>
-      <td style="width:32px">XII</td>
+      <td class="w-8">V<sup>+</sup></td>
+      <td class="w-5">VI<sup>&minus;</sup></td>
+      <td class="w-5">VI</td>
+      <td class="w-6">VI<sup>+</sup></td>
+      <td class="w-8">VII<sup>&minus;</sup></td>
+      <td class="w-8">VII</td>
+      <td class="w-12">VII<sup>+</sup></td>
+      <td class="w-12">VIII<sup>&minus;</sup></td>
+      <td class="w-11">VIII</td>
+      <td class="w-11">VIII<sup>+</sup></td>
+      <td class="w-10">IX<sup>&minus;</sup></td>
+      <td class="w-10">IX</td>
+      <td class="w-12">IX<sup>+</sup></td>
+      <td class="w-12">X<sup>&minus;</sup></td>
+      <td class="w-10">X</td>
+      <td class="w-10">X<sup>+</sup></td>
+      <td class="w-10">XI<sup>&minus;</sup></td>
+      <td class="w-8">XI</td>
+      <td class="w-8">XI<sup>+</sup></td>
+      <td class="w-8">XII<sup>&minus;</sup></td>
+      <td class="w-8">XII</td>
     </tr>
   </table>
   <p><a href="waarderingen#Elbsandstein">Elbsandstein</a></p>
   <table>
     <tr>
-      <td style="width:32px">VIa</td>
-      <td style="width:32px">VIb</td>
-      <td style="width:32px">VIc</td>
-      <td style="width:32px">VIIa</td>
-      <td style="width:32px">VIIb</td>
-      <td style="width:32px">VIIc</td>
-      <td style="width:32px">VIIIa</td>
-      <td style="width:32px">VIIIb</td>
-      <td style="width:32px">VIIIc</td>
-      <td style="width:32px">IXa</td>
-      <td style="width:32px">IXb</td>
-      <td style="width:32px">IXc</td>
-      <td style="width:32px">Xa</td>
-      <td style="width:32px">Xb</td>
-      <td style="width:32px">Xc</td>
-      <td style="width:40px">XIa</td>
-      <td style="width:40px">XIb</td>
-      <td style="width:40px">XIc</td>
-      <td style="width:40px">XIIa</td>
-      <td style="width:32px">XIIb</td>
+      <td class="w-8">VIa</td>
+      <td class="w-8">VIb</td>
+      <td class="w-8">VIc</td>
+      <td class="w-8">VIIa</td>
+      <td class="w-8">VIIb</td>
+      <td class="w-8">VIIc</td>
+      <td class="w-8">VIIIa</td>
+      <td class="w-8">VIIIb</td>
+      <td class="w-8">VIIIc</td>
+      <td class="w-8">IXa</td>
+      <td class="w-8">IXb</td>
+      <td class="w-8">IXc</td>
+      <td class="w-8">Xa</td>
+      <td class="w-8">Xb</td>
+      <td class="w-8">Xc</td>
+      <td class="w-10">XIa</td>
+      <td class="w-10">XIb</td>
+      <td class="w-10">XIc</td>
+      <td class="w-10">XIIa</td>
+      <td class="w-8">XIIb</td>
     </tr>
   </table>
   <p><a href="waarderingen#YDS">Amerikaans</a></p>
   <table>
     <tr>
-      <td style="width:32px">5<span class="decp">.</span>8</td>
-      <td style="width:32px">5<span class="decp">.</span>9</td>
-      <td style="width:32px">5<span class="decp1">.</span>10a</td>
-      <td style="width:28px">5<span class="decp1">.</span>10b</td>
-      <td style="width:28px">5<span class="decp1">.</span>10c</td>
-      <td style="width:28px">5<span class="decp1">.</span>10d</td>
-      <td style="width:28px">5<span class="decp1">.</span>11a</td>
-      <td style="width:28px">5<span class="decp1">.</span>11b</td>
-      <td style="width:28px">5<span class="decp1">.</span>11c</td>
-      <td style="width:28px">5<span class="decp1">.</span>11d</td>
-      <td style="width:28px">5<span class="decp1">.</span>12a</td>
-      <td style="width:32px">5<span class="decp1">.</span>12b</td>
-      <td style="width:32px">5<span class="decp1">.</span>12c</td>
-      <td style="width:32px">5<span class="decp1">.</span>12d</td>
-      <td style="width:32px">5<span class="decp1">.</span>13a</td>
-      <td style="width:32px">5<span class="decp1">.</span>13b</td>
-      <td style="width:32px">5<span class="decp1">.</span>13c</td>
-      <td style="width:32px">5<span class="decp1">.</span>13d</td>
-      <td style="width:32px">5<span class="decp1">.</span>14a</td>
-      <td style="width:32px">5<span class="decp1">.</span>14b</td>
-      <td style="width:32px">5<span class="decp1">.</span>14c</td>
-      <td style="width:32px">5<span class="decp1">.</span>14d</td>
-      <td style="width:32px">5<span class="decp1">.</span>15a</td>
-      <td style="width:32px">5<span class="decp1">.</span>15b</td>
-      <td style="width:32px">5<span class="decp1">.</span>15c</td>
-      <td style="width:32px">5<span class="decp1">.</span>15d</td>
+      <td class="w-8">5<span class="decp">.</span>8</td>
+      <td class="w-8">5<span class="decp">.</span>9</td>
+      <td class="w-8">5<span class="decp1">.</span>10a</td>
+      <td class="w-7">5<span class="decp1">.</span>10b</td>
+      <td class="w-7">5<span class="decp1">.</span>10c</td>
+      <td class="w-7">5<span class="decp1">.</span>10d</td>
+      <td class="w-7">5<span class="decp1">.</span>11a</td>
+      <td class="w-7">5<span class="decp1">.</span>11b</td>
+      <td class="w-7">5<span class="decp1">.</span>11c</td>
+      <td class="w-7">5<span class="decp1">.</span>11d</td>
+      <td class="w-7">5<span class="decp1">.</span>12a</td>
+      <td class="w-8">5<span class="decp1">.</span>12b</td>
+      <td class="w-8">5<span class="decp1">.</span>12c</td>
+      <td class="w-8">5<span class="decp1">.</span>12d</td>
+      <td class="w-8">5<span class="decp1">.</span>13a</td>
+      <td class="w-8">5<span class="decp1">.</span>13b</td>
+      <td class="w-8">5<span class="decp1">.</span>13c</td>
+      <td class="w-8">5<span class="decp1">.</span>13d</td>
+      <td class="w-8">5<span class="decp1">.</span>14a</td>
+      <td class="w-8">5<span class="decp1">.</span>14b</td>
+      <td class="w-8">5<span class="decp1">.</span>14c</td>
+      <td class="w-8">5<span class="decp1">.</span>14d</td>
+      <td class="w-8">5<span class="decp1">.</span>15a</td>
+      <td class="w-8">5<span class="decp1">.</span>15b</td>
+      <td class="w-8">5<span class="decp1">.</span>15c</td>
+      <td class="w-8">5<span class="decp1">.</span>15d</td>
     </tr>
   </table>
   <p><a href="waarderingen#Ewbank">Australisch</a></p>
   <table>
     <tr>
-      <td style="width:32px">16</td>
-      <td style="width:32px">17</td>
-      <td style="width:32px">18</td>
-      <td style="width:32px">19</td>
-      <td style="width:32px">20</td>
-      <td style="width:32px">21</td>
-      <td style="width:32px">22</td>
-      <td style="width:32px">23</td>
-      <td style="width:32px">24</td>
-      <td style="width:32px">25</td>
-      <td style="width:32px">26</td>
-      <td style="width:32px">27</td>
-      <td style="width:40px">28</td>
-      <td style="width:40px">29</td>
-      <td style="width:40px">30</td>
-      <td style="width:40px">31</td>
-      <td style="width:32px">32</td>
-      <td style="width:32px">33</td>
-      <td style="width:32px">34</td>
-      <td style="width:32px">35</td>
-      <td style="width:32px">36</td>
-      <td style="width:32px">37</td>
-      <td style="width:32px">38</td>
+      <td class="w-8">16</td>
+      <td class="w-8">17</td>
+      <td class="w-8">18</td>
+      <td class="w-8">19</td>
+      <td class="w-8">20</td>
+      <td class="w-8">21</td>
+      <td class="w-8">22</td>
+      <td class="w-8">23</td>
+      <td class="w-8">24</td>
+      <td class="w-8">25</td>
+      <td class="w-8">26</td>
+      <td class="w-8">27</td>
+      <td class="w-10">28</td>
+      <td class="w-10">29</td>
+      <td class="w-10">30</td>
+      <td class="w-10">31</td>
+      <td class="w-8">32</td>
+      <td class="w-8">33</td>
+      <td class="w-8">34</td>
+      <td class="w-8">35</td>
+      <td class="w-8">36</td>
+      <td class="w-8">37</td>
+      <td class="w-8">38</td>
     </tr>
   </table>
   <p><a href="waarderingen#adjectival-grade">Engels</a></p>
   <table>
     <tr>
-      <td style="width:64px">HVS</td>
-      <td style="width:56px">E1</td>
-      <td style="width:56px">E2</td>
-      <td style="width:56px">E3</td>
-      <td style="width:56px">E4</td>
-      <td style="width:56px">E5</td>
-      <td style="width:56px">E6</td>
-      <td style="width:56px">E7</td>
-      <td style="width:48px">E8</td>
-      <td style="width:48px">E9</td>
-      <td style="width:48px">E10</td>
-      <td style="width:48px">E11</td>
-      <td style="width:48px">E12</td>
+      <td class="w-16">HVS</td>
+      <td class="w-14">E1</td>
+      <td class="w-14">E2</td>
+      <td class="w-14">E3</td>
+      <td class="w-14">E4</td>
+      <td class="w-14">E5</td>
+      <td class="w-14">E6</td>
+      <td class="w-14">E7</td>
+      <td class="w-12">E8</td>
+      <td class="w-12">E9</td>
+      <td class="w-12">E10</td>
+      <td class="w-12">E11</td>
+      <td class="w-12">E12</td>
     </tr>
   </table>
 </section>
@@ -185,84 +185,84 @@
   <p><a href="waarderingen#technical-grade">Engels</a></p>
   <table>
     <tr>
-      <td style="width:64px">5a</td>
-      <td style="width:80px">5b</td>
-      <td style="width:80px">5c</td>
-      <td style="width:96px">6a</td>
-      <td style="width:96px">6b</td>
-      <td style="width:96px">6c</td>
-      <td style="width:96px">7a</td>
-      <td style="width:96px">7b</td>
+      <td class="w-16">5a</td>
+      <td class="w-20">5b</td>
+      <td class="w-20">5c</td>
+      <td class="w-24">6a</td>
+      <td class="w-24">6b</td>
+      <td class="w-24">6c</td>
+      <td class="w-24">7a</td>
+      <td class="w-24">7b</td>
     </tr>
   </table>
   <p><a href="waarderingen#cotation-bloc">Bloc (Frans)</a></p>
-  <table style="width:768px;">
+  <table>
     <tr>
-      <td style="width:32px">3<sup>+</sup></td>
-      <td style="width:32px">4</td>
-      <td style="width:32px">4<sup>+</sup></td>
-      <td style="width:32px">5</td>
-      <td style="width:32px">5<sup>+</sup></td>
-      <td style="width:32px">6a</td>
-      <td style="width:32px">6a<sup>+</sup></td>
-      <td style="width:32px">6b</td>
-      <td style="width:32px">6b<sup>+</sup></td>
-      <td style="width:32px">6c</td>
-      <td style="width:32px">6c<sup>+</sup></td>
-      <td style="width:32px">7a</td>
-      <td style="width:32px">7a<sup>+</sup></td>
-      <td style="width:32px">7b</td>
-      <td style="width:32px">7b<sup>+</sup></td>
-      <td style="width:32px">7c</td>
-      <td style="width:32px">7c<sup>+</sup></td>
-      <td style="width:32px">8a</td>
-      <td style="width:32px">8a<sup>+</sup></td>
-      <td style="width:32px">8b</td>
-      <td style="width:32px">8b<sup>+</sup></td>
-      <td style="width:32px">8c</td>
-      <td style="width:32px">8c+<sup>+</sup></td>
-      <td style="width:32px">9a</td>
+      <td class="w-8">3<sup>+</sup></td>
+      <td class="w-8">4</td>
+      <td class="w-8">4<sup>+</sup></td>
+      <td class="w-8">5</td>
+      <td class="w-8">5<sup>+</sup></td>
+      <td class="w-8">6a</td>
+      <td class="w-8">6a<sup>+</sup></td>
+      <td class="w-8">6b</td>
+      <td class="w-8">6b<sup>+</sup></td>
+      <td class="w-8">6c</td>
+      <td class="w-8">6c<sup>+</sup></td>
+      <td class="w-8">7a</td>
+      <td class="w-8">7a<sup>+</sup></td>
+      <td class="w-8">7b</td>
+      <td class="w-8">7b<sup>+</sup></td>
+      <td class="w-8">7c</td>
+      <td class="w-8">7c<sup>+</sup></td>
+      <td class="w-8">8a</td>
+      <td class="w-8">8a<sup>+</sup></td>
+      <td class="w-8">8b</td>
+      <td class="w-8">8b<sup>+</sup></td>
+      <td class="w-8">8c</td>
+      <td class="w-8">8c+<sup>+</sup></td>
+      <td class="w-8">9a</td>
     </tr>
   </table>
   <p><a href="waarderingen#V-scale">Amerikaans/Australisch</a></p>
   <table>
     <tr>
-      <td style="width:64px">V0</td>
-      <td style="width:32px">V0+</td>
-      <td style="width:32px">V1</td>
-      <td style="width:32px">V2</td>
-      <td style="width:64px">V3</td>
-      <td style="width:48px">V4</td>
-      <td style="width:48px">V5</td>
-      <td style="width:48px">V6</td>
-      <td style="width:48px">V7</td>
-      <td style="width:48px">V8</td>
-      <td style="width:48px">V9</td>
-      <td style="width:32px">V10</td>
-      <td style="width:32px">V11</td>
-      <td style="width:32px">V12</td>
-      <td style="width:32px">V13</td>
-      <td style="width:32px">V14</td>
-      <td style="width:32px">V15</td>
-      <td style="width:32px">V16</td>
-      <td style="width:32px">V17</td>
+      <td class="w-16">V0</td>
+      <td class="w-8">V0+</td>
+      <td class="w-8">V1</td>
+      <td class="w-8">V2</td>
+      <td class="w-16">V3</td>
+      <td class="w-12">V4</td>
+      <td class="w-12">V5</td>
+      <td class="w-12">V6</td>
+      <td class="w-12">V7</td>
+      <td class="w-12">V8</td>
+      <td class="w-12">V9</td>
+      <td class="w-8">V10</td>
+      <td class="w-8">V11</td>
+      <td class="w-8">V12</td>
+      <td class="w-8">V13</td>
+      <td class="w-8">V14</td>
+      <td class="w-8">V15</td>
+      <td class="w-8">V16</td>
+      <td class="w-8">V17</td>
     </tr>
   </table>
   <p><a href="waarderingen#cotation-traverse">Traversewaardering (Frans)</a></p>
   <table>
     <tr>
-      <td style="width:32px">3</td>
-      <td style="width:64px">4</td>
-      <td style="width:64px">5</td>
-      <td style="width:32px">6a</td>
-      <td style="width:32px">6b</td>
-      <td style="width:32px">6c</td>
-      <td style="width:32px">7a</td>
-      <td style="width:64px">7b</td>
-      <td style="width:64px">7c</td>
-      <td style="width:64px">8a</td>
-      <td style="width:96px">8b</td>
-      <td style="width:96px">8c</td>
+      <td class="w-8">3</td>
+      <td class="w-16">4</td>
+      <td class="w-16">5</td>
+      <td class="w-8">6a</td>
+      <td class="w-8">6b</td>
+      <td class="w-8">6c</td>
+      <td class="w-8">7a</td>
+      <td class="w-16">7b</td>
+      <td class="w-16">7c</td>
+      <td class="w-16">8a</td>
+      <td class="w-24">8b</td>
+      <td class="w-24">8c</td>
     </tr>
   </table>
 </section>
